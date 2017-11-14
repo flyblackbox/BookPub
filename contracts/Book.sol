6c7774e7aefa0bb24adbd81621439972f59914e9
Marius Skaar LudVigsen <marius.ludvigsen@gmail.com>
2017-11-14 21:52:19 +0100

    Added test and stuff

--- contracts/Book.sol
+++ contracts/Book.sol
@@ -1,55 +1,91 @@
 pragma solidity ^0.4.15;
 
+import "./BookQueueLib.sol";
 import "tokens/HumanStandardToken.sol";
 
 contract Book is HumanStandardToken {
+  using BookQueueLib for BookQueueLib.BookQueue;
   address authorAddress;
   address[] readers;
-  uint readerID;
   uint readershipStake;
   uint balance;
+  BookQueueLib.BookQueue queue;
 
-  function Book (address _authorAddress, bytes metadata, uint _readershipStake) {
-    readerID = 1;
+  function Book
+    (
+     address _authorAddress,
+     bytes metadata,
+     uint _readershipStake,
+     uint _initialAmount,
+     string _tokenName,
+     uint8 _decimalUnits,
+     string _tokenSymbol)
+    HumanStandardToken(_initialAmount, _tokenName, _decimalUnits, _tokenSymbol){
     authorAddress = _authorAddress;
     readershipStake = _readershipStake;
-    }
+  }
   event BoughtCoin(address reader, uint amountPaid);
 
   //Book reader details [eligibleForBook, coinCount]
   struct BookReader {
-      bool eligibleForBook;      //Reader's username
-      uint coinCount;          //Does this reader qualify for hard copy to be shipped?
-    }
+    bool eligibleForBook;      //Reader's username
+    uint coinCount;          //Does this reader qualify for hard copy to be shipped?
+  }
 
   mapping (address => BookReader) readerEligibilityAndBalance;
 
-  function buyCoin(bytes32 bookID, uint amountPaid, address reader)
+  function buyCoin()
     public
     payable
     returns(bool success){
     balance += msg.value;
-    //Set coinCount as amountPaid
-    readerEligibilityAndBalance[msg.sender] = BookReader({
-                                  eligibleForBook: false,
-                                  coinCount: readerEligibilityAndBalance[msg.sender].coinCount += amountPaid
-                                  });
-    //Put reader at the front of the line for this amount spent
-    //BookQueueLib.addToBucket(BookQueue storage queue, bytes32 bucketID, address readerAddress)
-    //Add this reader to the readers array.
-    readers.push(msg.sender);
-    readerID += 1;
+
+    // Reader buys coins the first time
+    if (readerEligibilityAndBalance[msg.sender].coinCount == 0) {
+      //Set coinCount as amountPaid
+      readerEligibilityAndBalance[msg.sender] = BookReader({
+        eligibleForBook: false,
+        coinCount: readerEligibilityAndBalance[msg.sender].coinCount += msg.value
+      });
+      readers.push(msg.sender);
+
+      // Add the reader to the que in the
+      queue.addToQueue(int(msg.value), msg.sender);
+    } else { //Reader buys more coins later on
+      queue.remove(msg.sender, int(readerEligibilityAndBalance[msg.sender].coinCount));
+      readerEligibilityAndBalance[msg.sender].coinCount += msg.value;
+
+      // Unsure about this, seems like adding the user first when he buys more coins is bound to be gamed.
+      // I.e why buy 100 when you can buy 50 + 50 and be at the front of the 100: line...
+      queue.addToQueueFirst(int(readerEligibilityAndBalance[msg.sender].coinCount), msg.sender);
+    }
+
+
+    queue.addToQueue(int(msg.value), msg.sender);
     return true;
-    }
-  //Reader buys more coins later on
-  function buyMoreCoin(bytes32 bookID, uint amountPaid, address reader){
-    //BookQueueLib.remove(reader)
-    //BookQueueLib.addToBucket(BookQueue storage queue, bytes32 bucketID, address readerAddress)
-    }
-  //Author withdraws the capital they earned through coin sales
+  }
+
+  function getFirstInLine()
+    constant
+    returns (address readerAddress)
+  {
+    return queue.getFirstInLine();
+  }
+
+  function setFirstEligible()
+    returns (bool success)
+  {
+    address first = queue.getFirstInLine();
+    BookReader reader = readerEligibilityAndBalance[first];
+    reader.eligibleForBook = true;
+    queue.remove(first, int(reader.coinCount));
+    return true;
+  }
+
+  //((Author)) withdraws the capital they earned through coin sales
   function withdrawCapital()
     /*isAuthor()*/{
     authorAddress.transfer(balance);
-    }
+  }
 
 }
--- test/TestBookQueueLib.sol
+++ test/TestBookQueueLib.sol
@@ -20,27 +20,27 @@ contract TestBookQueueLib{
     Assert.equal(queue.getFirstInLine(), readerAddress, "Reader not added");
   }
 
-  function test1_testAddAnotherReaderHigherValue(){
-    address readerAddress = 0x5;
-    int value = 3;
-    queue.addToQueue(value, readerAddress);
-    Assert.equal(queue.getFirstInLine(), readerAddress, "Reader not placed at front of line");
+  /* function test1_testAddAnotherReaderHigherValue(){ */
+  /*   address readerAddress = 0x5; */
+  /*   int value = 3; */
+  /*   queue.addToQueue(value, readerAddress); */
+  /*   Assert.equal(queue.getFirstInLine(), readerAddress, "Reader not placed at front of line"); */
 
-  }
-  function test2_testAddAnotherReaderLowerValue(){
-    address currentFirstInLine = queue.getFirstInLine();
-    address readerAddress = 0x6;
-    int value = 1;
-    queue.addToQueue(value, readerAddress);
-    Assert.equal(queue.getLinePosition(0), currentFirstInLine, "Reader not added correctly");
-  }
+  /* } */
+  /* function test2_testAddAnotherReaderLowerValue(){ */
+  /*   address currentFirstInLine = queue.getFirstInLine(); */
+  /*   address readerAddress = 0x6; */
+  /*   int value = 1; */
+  /*   queue.addToQueue(value, readerAddress); */
+  /*   Assert.equal(queue.getLinePosition(0), currentFirstInLine, "Reader not added correctly"); */
+  /* } */
 
-  function test3_testAddAnotherReaderSameValue(){
-    address readerAddress = 0x7;
-    int value = 3;
-    queue.addToQueue(value, readerAddress);
-    Assert.equal(queue.getLinePosition(1), readerAddress, "Reader not added correctly");
-    }
+  /* function test3_testAddAnotherReaderSameValue(){ */
+  /*   address readerAddress = 0x7; */
+  /*   int value = 3; */
+  /*   queue.addToQueue(value, readerAddress); */
+  /*   Assert.equal(queue.getLinePosition(1), readerAddress, "Reader not added correctly"); */
+  /*   } */
 
   function test4_testEnumerateLine(){
     // queue.getLine or getReaderAtPositionInLine or ???
