cf4a6f3d4c50b38e1afefd222b3b8e4fed6f8c51
johhonn <35551051+johhonn@users.noreply.github.com>
2018-01-25 14:05:26 -0500

    Add files via upload

--- test/BookPubTest.js
+++ test/BookPubTest.js
@@ -5,15 +5,15 @@ it("creates a book with readerstake of 1 ", function() {
     var instance;
     return BookPub.new( {
       from: accounts[0],
-       }) 
+       })
         .then(_instance => {
             instance = _instance;
            return instance.publishBook(1,"MYBOOK","BK","boardroom");
         })
         .then(() =>{
-            
-            return instance.StructRet.call(1);
-        })     
+
+            return instance.structRet.call(1);
+        })
         .then(resultValue => {
 		    console.log(resultValue);
             assert.equal(resultValue.toString(), N, "Readershipstake should be 1");
@@ -25,15 +25,15 @@ it("creates a book with a tokenname of MYBOOK", function() {
     var instance;
     return BookPub.new( {
       from: accounts[0],
-       }) 
+       })
         .then(_instance => {
             instance = _instance;
            return instance.publishBook(1,"MYBOOK","BK","boardroom");
         })
         .then(() =>{
-            
+
             return instance.nameret.call(1);
-        })     
+        })
         .then(resultValue => {
 		    console.log(resultValue);
             assert.equal(resultValue.toString(), Name, "tokenname should be MYBOOK");
@@ -45,68 +45,102 @@ it("creates a book with a tokensymbol of BK", function() {
     var instance;
     return BookPub.new( {
       from: accounts[0],
-       }) 
+       })
         .then(_instance => {
             instance = _instance;
             return instance.publishBook(1,"MYBOOK","BK","boardroom");
         })
         .then(() =>{
-            
+
             return instance.symbolret.call(1);
-        })     
-        .then(resultValue => {
-		    console.log(resultValue);
-            assert.equal(resultValue.toString(), Symbol, " symbol should be BK");
-            // Do not return anything on the last callback or it will believe there is an error.
-        });
-});		
-it(" BOOKID will increment with iterate with function call", function() {
-	var Symbol="BKK";
-    var instance;
-    return BookPub.new( {
-      from: accounts[0],
-       }) 
-        .then(_instance => {
-            instance = _instance;
-            return instance.publishBook(1,"MYBOOK","BK","boardroom");
-        }).then(() =>{
-            
-             return instance.publishBook(1,"MYBOOK","BKK","boardroom");
-        }).then(() =>{
-            
-            return instance.symbolret.call(2);
-        })     
+        })
         .then(resultValue => {
 		    console.log(resultValue);
             assert.equal(resultValue.toString(), Symbol, " symbol should be BK");
             // Do not return anything on the last callback or it will believe there is an error.
         });
 });
-	it(" Partner ID of 12 should be added", function() {
-	var ID=12;
+it(" BOOKID will increment with iterate with function call", function() {
+	var Symbol="BKK";
     var instance;
     return BookPub.new( {
       from: accounts[0],
-       }) 
+       })
         .then(_instance => {
             instance = _instance;
             return instance.publishBook(1,"MYBOOK","BK","boardroom");
         }).then(() =>{
-            
-             return instance.addPartner(1,12,10);
+
+             return instance.publishBook(1,"MYBOOK","BKK","boardroom");
         }).then(() =>{
-            
-            return instance.partner.call(1,0);
-        })     
+
+            return instance.symbolret.call(2);
+        })
         .then(resultValue => {
 		    console.log(resultValue);
-            assert.equal(resultValue, ID , " ID should be 12");
+            assert.equal(resultValue.toString(), Symbol, " symbol should be BK");
             // Do not return anything on the last callback or it will believe there is an error.
         });
-});			
-	
-	
-	
-	
-	
+});
+it(" modify book works", function() {
+	var goal=100;
+	var s=2;
+	var e=4;
+	var eligible=300;
+    var instance;
+    return BookPub.new( {
+      from: accounts[0],
+       })
+        .then(_instance => {
+            instance = _instance;
+            return instance.publishBook(1,"MYBOOK","BK","boardroom");
+        }).then(() =>{
+
+             return instance.modifyBook(1,100,2,4,300);
+        }).then(() =>{
+
+            return instance.getOtherparams.call(1);
+        })
+        .then(resultValue => {
+		   // console.log(resultValue);
+            assert.equal(resultValue[0].c.toString(), goal, " goal should be 100");
+						assert.equal(resultValue[1].toString(), s, " start should be 2");
+						assert.equal(resultValue[2].toString(), e, " end should be 4");
+						assert.equal(resultValue[3].toString(), eligible, " eligible should be 300");
+            // Do not return anything on the last callback or it will believe there is an error.
+        });
+		});
+				it(" add a partner works", function() {
+					var stake=100;
+
+				    var instance;
+				    return BookPub.new( {
+				      from: accounts[0],
+				       })
+				        .then(_instance => {
+				            instance = _instance;
+				            return instance.publishBook(1,"MYBOOK","BK","boardroom");
+				        }).then(() =>{
+
+				             return instance.addPartner(1,100);
+				        }).then(() =>{
+
+				            return instance.getPartnerShare.call(1,0);
+				        })
+				        .then(resultValue => {
+						    //console.log(resultValue);
+				            assert.equal(resultValue.toString(), stake, " gstake should be 100");
+
+				            // Do not return anything on the last callback or it will believe there is an error.
+				        });
+							});
+
+
+
+
+
+
+
+
+
 });
