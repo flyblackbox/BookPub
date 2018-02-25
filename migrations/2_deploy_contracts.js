5f10d9759e828db06d0305b6ae614dc910f7b1f3
johhonn <35551051+johhonn@users.noreply.github.com>
2018-02-25 15:08:13 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var GroveLib = artifacts.require('./GroveLib.sol');
-var BookQueueLib = artifacts.require('./BookQueueLib.sol');
+var OrderedStatisticTree = artifacts.require('./OrderedStatisticTree.sol');
+var BookQueueLib = artifacts.require('./BookQueueLib2.sol');
 var BookPub = artifacts.require('./BookPub.sol');
 var Book = artifacts.require('./Book.sol');
 var BookDistLib = artifacts.require('./BookDistLib.sol');
@@ -9,13 +9,13 @@ var Stoppable = artifacts.require('../contracts/Stoppable.sol');
 module.exports = function(deployer) {
   deployer.deploy(Owned);
   deployer.deploy(Stoppable);
-  deployer.deploy(GroveLib);
-  deployer.link(GroveLib, BookQueueLib);
+  deployer.deploy(OrderedStatisticTree);
+  deployer.link(OrderedStatisticTree, BookQueueLib);
   deployer.deploy(BookQueueLib);
   deployer.link(BookQueueLib, Book);
   deployer.link(BookQueueLib, BookPub);
   deployer.deploy(BookPub);
-  deployer.deploy(Book);
+  deployer.deploy(Book,'0x627306090abaB3A6e1400e9345bC60c78a8BEf57',100,100,100,100,100,"TestBook",10000,1,50,"TB");
   deployer.deploy(BookDistLib);
-  
+
 };
